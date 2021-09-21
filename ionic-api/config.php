f4e5349ae3d887f1cb6b4842d568439b1b2dff09
sudeshiuthpala <57354717+sudeshiuthpala@users.noreply.github.com>
2021-09-21 13:53:46 +0530

    Delete ionic-api directory

--- ionic-api/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // define('DB_NAME','cpdmf');
-    // define('DB_USER','root');
-    // define('DB_HOST','localhost');
-    // define('DB_PASSWORD','');
-
-    $mysqli=mysqli_connect('localhost','root','','cpdmf');
-
-?>
